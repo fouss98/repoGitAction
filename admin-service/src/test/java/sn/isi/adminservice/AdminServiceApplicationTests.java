1a3fe908b952f028eea6c07b98c0278003d66118
fouss98 <camarafousseinou6@gmail.com>
2023-01-09 20:58:48 +0000

    first commit

---
+++ admin-service/src/test/java/sn/isi/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.isi.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
